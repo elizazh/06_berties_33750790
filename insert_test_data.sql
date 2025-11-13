93b203fcfdb3568ea7285daa8511e3e6e8647889
elizazh <149685559+elizazh@users.noreply.github.com>
2025-11-13 01:49:01 +0000

    Create insert_test_data.sql

---
+++ insert_test_data.sql
@@ -0,0 +1,8 @@
+USE berties_books;
+
+INSERT INTO books (name, price) VALUES
+('Brighton Rock', 20.25),
+('Brave New World', 25.00),
+('Animal Farm', 12.99),
+('The Hobbit', 18.50),
+('The Catcher in the Rye', 21.00);
