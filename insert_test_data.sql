b2b73a8ac643a0a25be52c88ec3a9beffa8d4daf
elizazh <149685559+elizazh@users.noreply.github.com>
2025-11-15 03:05:53 +0000

    Update insert_test_data.sql

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -4,5 +4,5 @@ INSERT INTO books (name, price) VALUES
 ('Brighton Rock', 20.25),
 ('Brave New World', 25.00),
 ('Animal Farm', 12.99),
-('The Hobbit', 18.50),
-('The Catcher in the Rye', 21.00);
+('Dune', 28.50),
+('A Scanner Darkly', 14.75);
