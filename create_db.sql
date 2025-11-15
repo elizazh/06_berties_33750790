ce0dd91fa4ad5ba0ed8dd19a7dbae4457f89f748
elizazh <149685559+elizazh@users.noreply.github.com>
2025-11-15 18:07:19 +0000

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,13 +1,11 @@
--- Create DB and table (with IF NOT EXISTS as per lab)
 CREATE DATABASE IF NOT EXISTS berties_books;
 USE berties_books;
 
 CREATE TABLE IF NOT EXISTS books (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id   INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   price DECIMAL(5,2) UNSIGNED
 );
 
--- App user + privileges (Lab 6 asks to include these at end)
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop';
 GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
