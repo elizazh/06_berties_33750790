084f7ad0ba4b1163cb5dcd148f61add59bdb99b5
elizazh <149685559+elizazh@users.noreply.github.com>
2025-11-15 19:55:24 +0000

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,13 +1,8 @@
 -- create_db.sql
--- Creates the berties_books database, books table and app user
-
--- Create the database if it does not already exist
 CREATE DATABASE IF NOT EXISTS berties_books;
 
--- Use the database
 USE berties_books;
 
--- Create the books table if it does not already exist
 CREATE TABLE IF NOT EXISTS books (
   id INT AUTO_INCREMENT,
   name VARCHAR(50),
@@ -15,10 +10,9 @@ CREATE TABLE IF NOT EXISTS books (
   PRIMARY KEY (id)
 );
 
--- Create the application user if it does not already exist
+-- Create the application user (for Node app)
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop';
 
--- Give the application user access to the berties_books database
 GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
 
 FLUSH PRIVILEGES;
